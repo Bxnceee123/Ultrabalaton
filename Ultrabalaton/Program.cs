3ade72497d5c6a14c3c498769a04d03473c94768
Bxnceee123 <163414403+Bxnceee123@users.noreply.github.com>
2024-12-09 13:57:46 +0100

    Add project files.

---
+++ Ultrabalaton/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Ultrabalaton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Versenyző> versenyzők = new();
+            foreach (var sor in File.ReadAllLines("ub2017egyeni.txt").Skip(1))
+            {
+                versenyzők.Add(new Versenyző(sor));
+            }
+            Console.WriteLine($"3. feladat: Egyéni indulók: {versenyzők.Count} fő");
+        }
+    }
+}
---
+++ Ultrabalaton/Versenyző.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ultrabalaton
+{
+    internal class Versenyző
+    {
+        public string Nev { get; set; }
+        public int Rajtszam { get; set; }
+        public string Kategoria { get; set; }
+        public string Versenyido { get; set; }
+        public int TavSzazalek { get; set; }
+
+        public Versenyző(string sor)
+        {
+            string[] s = sor.Split(';');
+
+            Nev = s[0];
+            Rajtszam = int.Parse(s[1]);
+            Kategoria = s[2];
+            Versenyido = s[3];
+            TavSzazalek = int.Parse(s[4]);
+        }
+
+    }
+}
